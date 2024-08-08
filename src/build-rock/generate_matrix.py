8c6907cc7919847ed2ec1248d929eca8ee573920
Adrian Clay Lake <clay.lake@canonical.com>
2024-08-08 13:19:06 +0200

    small refactor of generate_matrix.py to allow for individual build jobs

--- src/build-rock/generate_matrix.py
+++ src/build-rock/generate_matrix.py
@@ -9,6 +9,7 @@ import json
 # - rename to something more descriptive
 # - refactor and add testing
 
+GH_SUPPORTED_ARCHS = {"amd64": "ubuntu-22.04", "arm64": "Ubuntu_ARM64_4C_16G_01"}
 
 parser = argparse.ArgumentParser()
 
@@ -19,37 +20,36 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-BUILD_WITH_LPCI = 0
-
 with open(f"{args.rockfile_directory}/rockcraft.yaml") as rf:
     rockcraft_yaml = yaml.safe_load(rf)
 
-platforms = rockcraft_yaml["platforms"]
+rock_platforms = rockcraft_yaml["platforms"]
+rock_name = rockcraft_yaml["name"]
 
 target_archs = []
-for platf, values in platforms.items():
+for platf, values in rock_platforms.items():
     if isinstance(values, dict) and "build-for" in values:
         target_archs += list(values["build-for"])
         continue
     target_archs.append(platf)
 
-print(f"Target architectures: {set(target_archs)}")
+runner_build_matrix = {"include": []}
+lpci_build_matrix = {"include": []}
 
-matrix = {"include": []}
-gh_supported_archs = {"amd64": "ubuntu-22.04", "arm64": "Ubuntu_ARM64_4C_16G_01"}
-if set(target_archs) - set(gh_supported_archs.keys()):
+# TODO: is there a reason we cannot build on both systems
+if set(target_archs) - set(GH_SUPPORTED_ARCHS.keys()):
     # Then there are other target archs, so we need to build in LP
-    matrix["include"].append(
-    {"architecture": "-".join(set(target_archs)), "runner": gh_supported_archs["amd64"]}
+    lpci_build_matrix["include"].append(
+    {"architecture": "-".join(set(target_archs))}
     )
-    BUILD_WITH_LPCI = 1
 else:
-    for runner_arch, runner_name in gh_supported_archs.items():
+    for runner_arch, runner_name in GH_SUPPORTED_ARCHS.items():
         if runner_arch in target_archs:
-            matrix["include"].append(
+            runner_build_matrix["include"].append(
             {"architecture": runner_arch, "runner": runner_name}
             )
-matrix_json = json.dumps(matrix)
+
 with open(os.environ["GITHUB_OUTPUT"], "a") as gh_out:
-    print(f"build-for={matrix_json}", file=gh_out)
-    print(f"build-with-lpci={BUILD_WITH_LPCI}", file=gh_out)
+    print(f"rock-name={rock_name}", file=gh_out)
+    print(f"runner-build-matrix={json.dumps(runner_build_matrix)}", file=gh_out)
+    print(f"lpci-build-matrix={json.dumps(lpci_build_matrix)}", file=gh_out)
