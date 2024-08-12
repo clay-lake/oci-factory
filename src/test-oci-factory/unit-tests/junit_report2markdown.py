2ed6fc59ab24fb77a0f054164dce52e4318a873d
Adrian Clay Lake <clay.lake@canonical.com>
2024-08-12 16:51:18 +0200

    adjust formatting and test trigger

--- src/test-oci-factory/unit-tests/junit_report2markdown.py
+++ src/test-oci-factory/unit-tests/junit_report2markdown.py
@@ -3,48 +3,50 @@ import xml.etree.ElementTree as ET
 
 
 def print_element(element, output = None):
-    print("```", file=output)
+   
     for key, value in element.attrib.items():
-        print(f"{key}: {value}")
+        print(" "*4, f"{key}: {value}")
     
     if element.text is not None:
         if content := element.text.strip():
-            print(f"text: \n{content}")
-
-    print("```", file=output)
+            print(" "*4, f"text: \n{content}")
 
 
-def print_result_pie_chart(root, output = None):
+
+def print_testsuite_pie_chart(testsuite, output = None):
 
     chart_data = {
-        "failed": int(root.attrib.get("failures", 0)),
-        "error": int(root.attrib.get("errors", 0)),
-        "skipped": int(root.attrib.get("skipped", 0)),
+        "failed": int(testsuite.attrib.get("failures", 0)),
+        "error": int(testsuite.attrib.get("errors", 0)),
+        "skipped": int(testsuite.attrib.get("skipped", 0)),
     }
 
-    chart_data["pass"] = int(root.attrib.get("tests",0)) - chart_data["failed"] - chart_data["error"] - chart_data["skipped"]
+    chart_data["pass"] = int(testsuite.attrib.get("tests",0)) - chart_data["failed"] - chart_data["error"] - chart_data["skipped"]
 
     print("```mermaid", file=output)
 
     # theme colors in order: pass, failed, error, skipped
     print("%%{init: {'theme': 'base', 'themeVariables': { 'pie1': '#0f0', 'pie2': '#f00', 'pie3': '#fa0', 'pie4': '#ff0'}}}%%", file=output)
 
-    print(f"pie title {root.attrib['name']} Results", file=output)
+    print(f"pie title {testsuite.attrib['name']} Results", file=output)
     for key, value in chart_data.items():
         print(f'"{key}" : {value}', file=output)
 
     print("```", file=output)
 
 
-def markdown_report(root, output = None):
+def print_testsuite_report(testsuite, output = None):
 
-    print_result_pie_chart(root, output)
+    print_testsuite_pie_chart(testsuite, output)
 
-    for testcase in root.findall('testcase'):
+    test_file = testsuite
+
+    for testcase in testsuite.findall('testcase'):
 
         print("<details>", file=output)
-        # print(f"<summary>{testcase.attrib['name']} - {testcase.attrib['classname']}</summary>", file=output)
-        print(f"<summary>{testcase.attrib['name'].replace('_', ' ').title()}</summary>", file=output)
+        test_name = testcase.attrib['name'].replace('_', ' ').title()
+        test_class =  testcase.attrib['classname']
+        print(f"<summary>{test_name} - {test_class}</summary>", file=output)
 
         print_element(testcase)
         
@@ -74,5 +76,5 @@ if __name__ == "__main__":
     root = tree.getroot()
     
     for testsuite in root.findall("testsuite"):
-        markdown_report(testsuite, output=sys.stdout)
+        print_testsuite_report(testsuite, output=sys.stdout)
 
