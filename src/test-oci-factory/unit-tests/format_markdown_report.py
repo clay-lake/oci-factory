d6e012c44afe3942a73655808b9a5f5f7e781beb
Adrian Clay Lake <clay.lake@canonical.com>
2024-08-20 14:37:58 +0200

    cleaned up unit test reporting

--- src/test-oci-factory/unit-tests/format_markdown_report.py
+++ src/test-oci-factory/unit-tests/format_markdown_report.py
@@ -6,52 +6,55 @@ import json
 Generate markdown from a JUnit XML report for $GITHUB_STEP_SUMMARY 
 """
 
-def print_element(element, output = None):
+
+def print_element(element, output=None):
     """Generically display attrs and text of a element"""
     # TODO: add better formatting for children
-    print(f"<pre>", file = output)
+    print(f"<pre>", file=output)
 
     for key, value in element.attrib.items():
-        print(f"{key}: {value}", file = output)
-    
+        print(f"{key}: {value}", file=output)
+
     if element.text is not None:
         if content := element.text.strip():
-            print(f"text: \n{content}", file = output)
+            print(f"text: \n{content}", file=output)
 
-    print(f"</pre>", file = output)
+    print(f"</pre>", file=output)
 
-def print_testsuite_pie_chart(testsuite, output = None):
+
+def print_testsuite_pie_chart(testsuite, output=None):
     """Generate a pie chart showing test status from testsuite element"""
 
-    failed_tests = int(testsuite.attrib.get("failures", 0)) 
-    error_tests = int(testsuite.attrib.get("error", 0)) 
-    skipped_tests = int(testsuite.attrib.get("skipped", 0)) 
-    total_tests = int(testsuite.attrib.get("tests",0))
+    failed_tests = int(testsuite.attrib.get("failures", 0))
+    error_tests = int(testsuite.attrib.get("error", 0))
+    skipped_tests = int(testsuite.attrib.get("skipped", 0))
+    total_tests = int(testsuite.attrib.get("tests", 0))
+
+    # passed test has to be inferred
     pass_tests = total_tests - failed_tests - error_tests - skipped_tests
 
     #   name,       value,          colour,     default_order
     chart_data = [
-        ("failed",  failed_tests,   "#f00",     1),
-        ("error",   error_tests,    "#fa0",     2),
-        ("skipped", skipped_tests,  "#ff0",     3),
-        ("pass",    pass_tests,     "#0f0",     4)
-    ] 
+        ("failed", failed_tests, "#f00", 1),
+        ("error", error_tests, "#fa0", 2),
+        ("skipped", skipped_tests, "#ff0", 3),
+        ("pass", pass_tests, "#0f0", 4),
+    ]
     # note: default_order ensure color match if two wedges have the exact same value
 
     # filter out wedges with 0 width
     chart_data = list(filter(lambda w: w[1] != 0, chart_data))
 
     # sort by value, then default order so colors match what we expect
-    chart_data = list(
-        sorted(chart_data, 
-               key = lambda w: (w[1], w[3]),
-               reverse=True)
-        )
+    chart_data = list(sorted(chart_data, key=lambda w: (w[1], w[3]), reverse=True))
 
-    theme_dict = {'theme': 'base', 
-                    'themeVariables': {f'pie{n+1}': w[2] for n, w in enumerate(chart_data)}
-                    }
+    # create a theme
+    theme_dict = {
+        "theme": "base",
+        "themeVariables": {f"pie{n+1}": w[2] for n, w in enumerate(chart_data)},
+    }
 
+    ## begin printing piechart
     print("```mermaid", file=output)
 
     # theme colors in order: pass, failed, error, skipped
@@ -63,6 +66,7 @@ def print_testsuite_pie_chart(testsuite, output = None):
 
     print("```", file=output)
 
+
 def get_testcase_status(testcase):
     """Get status for individual testcase element status"""
 
@@ -72,18 +76,18 @@ def get_testcase_status(testcase):
         return ":warning:"
     elif testcase.find("skipped") is not None:
         return ":information_source:"
-    else: # passed
+    else:  # passed
         return ":white_check_mark:"
-    
-def print_header(testsuite, output = None):
+
+
+def print_header(testsuite, output=None):
     passed = not (testsuite.attrib.get("failures") or testsuite.attrib.get("errors"))
     status = ":white_check_mark:" if passed else ":x:"
 
     print(f"# {status} {testsuite.attrib['name']}", file=output)
 
 
-
-def print_testsuite_report(testsuite, output = None):
+def print_testsuite_report(testsuite, output=None):
     """Print complete testsuite element Report"""
 
     print_header(testsuite, output)
@@ -95,13 +99,17 @@ def print_testsuite_report(testsuite, output = None):
     print_element(testsuite, output)
 
     # print each test case in collapsable section
-    for testcase in testsuite.findall('testcase'):
+    for testcase in testsuite.findall("testcase"):
 
         print("<details>", file=output)
         test_status = get_testcase_status(testcase)
-        test_name = testcase.attrib['name'].replace('_', ' ').title()
-        test_class =  testcase.attrib['classname']
-        print(f"<summary>{test_status} {test_name} - {test_class}</summary>", file=output)
+        test_name = (
+            testcase.attrib["name"].replace("_", " ").title()
+        )  # make the title look better
+        test_class = testcase.attrib["classname"]
+        print(
+            f"<summary>{test_status} {test_name} - {test_class}</summary>", file=output
+        )
 
         for child in testcase.iter():
             print(f"<i>{child.tag}</i>", file=output)
@@ -122,17 +130,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--input-junit",
-        help="Path to JUnit XML Report",
-        required=True,
-        type=str
+        "--input-junit", help="Path to JUnit XML Report", required=True, type=str
     )
 
     args = parser.parse_args()
 
-
     tree = ET.parse(args.input_junit)
     root = tree.getroot()
     print_junit_report(root, sys.stdout)
-    
-
---
+++ src/test-oci-factory/unit-tests/test_format_markdown_report.py
@@ -0,0 +1,39 @@
+#! /bin/env python3
+
+import pytest
+import xml.etree.ElementTree as ET
+from pathlib import Path
+from io import StringIO
+
+import format_markdown_report as report
+
+
+@pytest.fixture
+def sample_failure():
+
+    sample_path = Path(__file__).parent / "data/sample_failure.xml"
+
+    tree = ET.parse(sample_path)
+    root = tree.getroot()
+    return root
+
+
+@pytest.fixture
+def str_buff():
+
+    with StringIO() as buffer:
+        yield buffer
+
+
+def test_print_junit_report_redirection(sample_failure, str_buff, capsys):
+
+    report.print_junit_report(sample_failure, str_buff)
+    report.print_junit_report(sample_failure, None)  # print to stdout
+
+    str_buff.seek(0)
+    str_buff_content = str_buff.read()
+
+    captured = capsys.readouterr()
+    stdout_content = captured.out
+
+    assert stdout_content == str_buff_content, "printing to multiple locations"
