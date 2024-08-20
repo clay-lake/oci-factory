bcfa2d166eef15f3b1b91e1c5096045d47ffc440
Adrian Clay Lake <clay.lake@canonical.com>
2024-08-20 16:09:10 +0200

    cleanup report unit tests

--- src/test-oci-factory/pytest/test_format_markdown_report.py
+++ src/test-oci-factory/pytest/test_format_markdown_report.py
@@ -5,14 +5,12 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 from io import StringIO
 
-import notamodule
-
 import format_markdown_report as report
 
 
 @pytest.fixture
 def sample_failure():
-
+    """Load ET of sample junit xml report with failure"""
     sample_path = Path(__file__).parent / "data/sample_failure.xml"
 
     tree = ET.parse(sample_path)
@@ -22,15 +20,16 @@ def sample_failure():
 
 @pytest.fixture
 def str_buff():
-
+    """String IO fixture for simulating a filehandle"""
     with StringIO() as buffer:
         yield buffer
 
 
 def test_print_junit_report_redirection(sample_failure, str_buff, capsys):
+    """Ensure that the report is entirely redirected when needed"""
 
     report.print_junit_report(sample_failure, str_buff)
-    report.print_junit_report(sample_failure, None)  # print to stdout
+    report.print_junit_report(sample_failure, None)  # print report to stdout
 
     str_buff.seek(0)
     str_buff_content = str_buff.read()
@@ -38,10 +37,5 @@ def test_print_junit_report_redirection(sample_failure, str_buff, capsys):
     captured = capsys.readouterr()
     stdout_content = captured.out
 
-    assert stdout_content == str_buff_content, "printing to multiple locations"
-
-
-def test_assert_false():
-
-    assert False, "Hello World"
+    assert stdout_content == str_buff_content, "Printing to multiple locations."
 
